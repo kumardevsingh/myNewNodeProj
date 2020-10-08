f4c5ee636aa04d4e1c8423688cad0ea95fbcbbbd
kumardevsingh <kumardevsingh@gmail.com>
2020-10-09 00:36:21 +0530

    product add using SQLize

--- util/database.js
+++ util/database.js
@@ -1,8 +1,8 @@
 const Sequelize = require('sequelize');
 
 const sequelize = new Sequelize('node-complete', 'root', 'Dev@12345', {
-    dialect: 'mysql',
-    host: 'localhost'
+  dialect: 'mysql',
+  host: 'localhost'
 });
 
 module.exports = sequelize;
